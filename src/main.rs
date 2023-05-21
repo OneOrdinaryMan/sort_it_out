9e57d3c7e6f66fa06411d43d545456223c78965a
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-05-21 17:29:53 +0530

    Merge Sort done

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,54 @@ impl<T: Ord + Copy> SortStruct<T> {
             }
         }
     }
+    fn merge_sort(&mut self) {
+        SortStruct::merge_sort_recurssion(&mut self.vector);
+    }
+    fn merge_sort_recurssion(input: &mut Vec<T>) {
+        if input.len() <= 1 {
+            return;
+        } else {
+            let middle = input.len() / 2;
+            let mut left_slice = Vec::new();
+            let mut right_slice = Vec::new();
+            for i in 0..input.len() {
+                if i < middle {
+                    left_slice.push(input[i]);
+                } else {
+                    right_slice.push(input[i]);
+                }
+            }
+            SortStruct::merge_sort_recurssion(&mut left_slice);
+            SortStruct::merge_sort_recurssion(&mut right_slice);
+            SortStruct::merge(&mut left_slice, &mut right_slice, input);
+        }
+    }
+    fn merge(left_slice: &mut Vec<T>, right_slice: &mut Vec<T>, input: &mut Vec<T>) {
+        let l = input.len() / 2;
+        let r = input.len() - l;
+        let (mut i, mut j, mut k) = (0, 0, 0);
+        while i < l && j < r {
+            if left_slice[i] <= right_slice[j] {
+                input[k] = left_slice[i];
+                i += 1;
+                k += 1;
+            } else {
+                input[k] = right_slice[j];
+                j += 1;
+                k += 1;
+            }
+        }
+        while i < l {
+            input[k] = left_slice[i];
+            i += 1;
+            k += 1;
+        }
+        while j < r {
+            input[k] = right_slice[j];
+            j += 1;
+            k += 1;
+        }
+    }
 }
 fn main() {}
 #[cfg(test)]
@@ -49,8 +97,8 @@ mod tests {
         let sorted_vector_2 = vec![1, 2, 3, 10];
         let mut sorter_1 = SortStruct::new(test_vector_1);
         let mut sorter_2 = SortStruct::new(test_vector_2);
-        sorter_1.insertion_sort();
-        sorter_2.insertion_sort();
+        sorter_1.merge_sort();
+        sorter_2.merge_sort();
         assert_eq!(sorter_1.vector, sorted_vector_1);
         assert_eq!(sorter_2.vector, sorted_vector_2);
     }
