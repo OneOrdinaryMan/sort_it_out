31127321e1cffc209b3cfd93d30b908c6d3b07bc
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-05-24 19:52:24 +0530

    Abstracted the func to do variated generics later

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 use sort_it_out::sorter::SortStruct;
-use std::io;
+use std::{fmt::Display, io};
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
 struct Cli {
@@ -10,6 +10,32 @@ struct Cli {
     #[arg(short, long)]
     sort: Option<String>,
 }
+fn sort_function<T: Copy + PartialOrd + Display>(
+    input: Vec<T>,
+    sort: Option<String>,
+    reverse: bool,
+) {
+    let mut sorter_1 = SortStruct::new(input);
+    match sort {
+        Some(value) => match value.as_str() {
+            "bubble" => sorter_1.bubble_sort(),
+            "selection" => sorter_1.selection_sort(),
+            "insertion" => sorter_1.insertion_sort(),
+            "merge" => sorter_1.merge_sort(),
+            "quick" => sorter_1.quick_sort(),
+            _ => {
+                println!("Cannot find the sort method.");
+                std::process::exit(1);
+            }
+        },
+        None => sorter_1.quick_sort(),
+    }
+    if reverse {
+        sorter_1.reverse();
+    }
+    sorter_1.print_vector();
+}
+
 fn main() {
     let cli = Cli::parse();
 
@@ -26,25 +52,6 @@ fn main() {
         }
     }
 
-    let input = input.iter().map(|x| x.as_str()).collect();
-
-    let mut sorter_1 = SortStruct::new(input);
-    match cli.sort {
-        Some(value) => match value.as_str() {
-            "bubble" => sorter_1.bubble_sort(),
-            "selection" => sorter_1.selection_sort(),
-            "insertion" => sorter_1.insertion_sort(),
-            "merge" => sorter_1.merge_sort(),
-            "quick" => sorter_1.quick_sort(),
-            _ => {
-                println!("Cannot find the sort method.");
-                std::process::exit(1);
-            }
-        },
-        None => sorter_1.quick_sort(),
-    }
-    if cli.reverse {
-        sorter_1.reverse();
-    }
-    sorter_1.print_vector();
+    let input: Vec<&str> = input.iter().map(|x| x.as_str()).collect();
+    sort_function(input, cli.sort, cli.reverse);
 }
